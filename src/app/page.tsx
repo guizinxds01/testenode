207df51486b5093e9216f831bd713ec027961395
guizinxds01 <guilhermevitorino658@gmail.com>
2024-11-26 17:07:22 -0300

    Primeiro commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import './page.module.css'
+
+export default function Home() {
+  return (
+    
+    <div className='topo'><h1>teste</h1>
+    
+    <button className="pika">aperte</button>
+    </div>
+    
+  
+
+  );
+}
+
